3e4f6546f172e55bc762b021dc1f752d9577caec
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-02-09 20:49:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$json = file_get_contents('https://exer-json.herokuapp.com/json.php');
+
+$data = json_decode($json,true);
+$list = $data['profile'];
+?>
+<h1> BTS members </h1>
+<?php
+foreach($list as $value){
+    ?>
+    <ul>
+        <li>Real Name: <?php echo $value['realname'];?></li>
+        <li>Stage Name: <?php echo $value['stagename'];?></li>
+        <li>Position: <?php echo $value['position'];?></li>
+        <li>Bloodtype: <?php echo $value['btype'];?></li>
+    </ul>
+ 
+<?php
+}
+?>
